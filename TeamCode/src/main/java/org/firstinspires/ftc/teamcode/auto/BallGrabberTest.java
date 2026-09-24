c7ed83bf39d571e9fa0e00f5166e0ed7336d3411
ProBotiX Bladel <probotixbladel@users.noreply.github.com>
2026-09-24 13:37:41 +0200

    test2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BallGrabberTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BallGrabberTest.java
@@ -38,7 +38,6 @@ public class BallGrabberTest extends OpMode {
 
 
     public void autonomousPathUpdate() {
-        //test
         if(!follower.isBusy()) {
             Pose target = comps.limelight.getBallPoses().get(0);
             double heading = Math.atan2(follower.pose().y() - target.y(), follower.pose().x() - target.x());
